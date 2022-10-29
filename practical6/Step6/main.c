04b85113d18b48783ca4e436142edf0539406db5
kapher <67718104+kapher@users.noreply.github.com>
2022-10-29 11:46:15 +0100

    Codes for Practical 6

---
+++ practical6/Step6/main.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(void){
+
+  int n=5, p=3, q=4;
+  double A[n][p], B[p][q], C[n][q];
+  int i,j,k;//Loop indices
+  
+  //Initialise A, B and C matrices
+  for(i=0; i<n; i++){
+    for(j=0; j<p; j++){
+      A[i][j]=i+j;
+    }
+  }
+  for(i=0; i<p; i++){
+    for(j=0; j<q; j++){
+      B[i][j]=i-j;
+    }
+  }
+
+
+/*  for(i=0; i<n; i++){
+    for(j=0; j<q; j++){
+      for(k=0; k<p; k++){
+        C[i][j]=C[i][j]+A[i][k]*B[k][j];
+      }
+    }
+  } */
+  matmult(n,p,q,A,B,C);
+  
+  //Print out the matrices
+  printf("\nThis is maytrix A:\n");
+  for(i=0; i<n; i++){
+    for(j=0; j<p; j++){
+      printf("%3.0f ", A[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nThis is maytrix B:\n");
+  for(i=0; i<p; i++){
+    for(j=0; j<q; j++){
+      printf("%3.0f ", B[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nThis is maytrix C:\n");
+  for(i=0; i<n; i++){
+    for(j=0; j<q; j++){
+      printf("%3.0f ", C[i][j]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
---
+++ practical6/Structs/example.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+  //Struc declaration
+  struct Person{
+    char namesurname[100];
+    int age;
+    float height, weight;
+  };
+
+  //struct variables
+  struct Person person1={"John Smith", 50, 165.78, 80.5};
+  //pointer to structure
+  struct Person *personptr;
+  personptr=&person1;
+  
+  //Printing
+  printf("\nDisplaying all information:\n");
+  printf("Name: %s\n", personptr->namesurname);
+  printf("Age=%d, Height=%f, Weight: %f\n\n", person1.age, person1.height, personptr->weight);
+
+  return 0;
+}
+  
+
